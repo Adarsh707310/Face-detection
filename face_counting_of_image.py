57d76e5de97dcff6a0c4434167d34ee8b9df80b8
Adarsh707310 <43859389+Adarsh707310@users.noreply.github.com>
2020-05-06 15:44:48 +0530

    Update and rename adarsh.py to face_counting_of_image.py

---
+++ face_counting_of_image.py
@@ -0,0 +1,10 @@
+import cv2
+#read the input image
+img = cv2.imread('test.png')
+
+# Display 
+cv2.imshow('img',img) 
+# so in video it says that opencv comes with train and detect of if you want you can train your classifier, i can use it for face detection with mask 
+
+
+
